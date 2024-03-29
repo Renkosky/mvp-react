85c6561ae1937430e224ae1c62f22f81f4968f88
Renko_sky <philsky28@gmail.com>
2024-03-29 16:47:10 +0800

    feat : add vite

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from "./core/react.js";
-import ReactDOM from "./core/ReactDOM.js";
-const app = React.createElement(
-  "div",
-  { id: "app" },
-  "Hello World from My React"
-);
-ReactDOM.createRoot(document.querySelector("#root")).render(app);
---
+++ vite-react-runner/app.jsx
@@ -0,0 +1,10 @@
+import React from "../core/React.js";
+
+//  const app = React.createElement(
+//   "div",
+//   { id: "app" },
+//   "Hello World from My React"
+// );
+const App = ()=><div>hi! react</div>
+
+export default App
